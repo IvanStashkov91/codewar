44dc572887a836fa7fd2af106ce5109ac94272f4
NeoKeen <ivanstashkov91@gmail.com>
2020-11-15 20:46:21 +0700

    polyndrome

--- polyndrome.py
+++ polyndrome.py
@@ -2,10 +2,34 @@ def reverse(text):
     return text[::-1]
 
 def is_palindrome(text):
+    #text = ''.join(text)
+    for i in forbidden:
+        text = text.replace(i, '')
+    return text == reverse(text)
+
+something = input('Введите текст: ')#.split()
+forbidden = ('!', '?', ',', '.', ':', ';', '-', ' ')
+print(something)
+
+if is_palindrome(something):
+    print("Да, это палиндром")
+else:
+    print("Нет, это не палиндром")
+
+
+###### или через генератор
+def reverse(text):
+    return text[::-1]
+
+def is_palindrome(text):
+    text = ''.join(i for i in something if not i in forbidden)
     return text == reverse(text)
 
 something = input('Введите текст: ')
-if (is_palindrome(something)):
+forbidden = ('!', '?', ',', '.', ':', ';', '-', ' ')
+print(something)
+
+if is_palindrome(something):
     print("Да, это палиндром")
 else:
     print("Нет, это не палиндром")
